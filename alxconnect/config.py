dd34409122f60238a27dc5c50982187a5fab5fcb
daniell-olaitan <daniell.olaitan@gmail.com>
2024-10-14 17:03:57 +0100

    Implement Log out functionality
    Implemented InvalidToken model to store revoked tokens to simulate
    logout functionality.

--- alxconnect/config.py
+++ alxconnect/config.py
@@ -1,10 +1,11 @@
 import os
 
-""" 
+"""
 ALXCONNECT CONFIGURATION FILE
 """
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
