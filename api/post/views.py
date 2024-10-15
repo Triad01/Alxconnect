4ca887425940568415846d593a30e5c645689bd0
daniell-olaitan <daniell.olaitan@gmail.com>
2024-10-15 09:55:44 +0100

    Implement pagination for posts and comments resources

--- api/post/views.py
+++ api/post/views.py
@@ -1,4 +1,5 @@
 from flask_restx import Namespace, Resource, abort
+from flask import request
 
 
 post_api = Namespace("posts", description="Post Api Routes")
@@ -9,11 +10,24 @@ class Get_and_Create_Post(Resource):
     def get(self):
         """Return all Posts"""
         from alxconnect.models import Post
-        posts = Post.query.all()
-        if not posts:
+
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('page_size', 10, type=int)
+
+        posts = Post.query.paginate(page=page, per_page=per_page)
+        if not posts.items:
             abort(404)
 
-        return [{post.id: post.to_json()}for post in posts]
+        return {
+            'posts': [post.to_json()for post in posts.items],
+            'page': posts.page,
+            'page_size': posts.per_page,
+            'total_page_items': len(posts.items),
+            'prev_page': posts.prev_num,
+            'next_page': posts.next_num,
+            'total': posts.total,
+            'total_pages': posts.pages
+        }, 200
 
 
 @post_api.route("/<int:post_id>", strict_slashes=False)
@@ -31,8 +45,26 @@ class Post_Comment(Resource):
     def get(self, post_id):
         """Return all comments on a post"""
         from alxconnect.models import Post
+
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('page_size', 10, type=int)
+
         post = Post.query.get(post_id)
         if not post:
             abort(404, message={"Error": "Post Not Found"})
         # print(post.comments[0])
-        return [{comment.id: comment.to_json()} for comment in post.comments]
+
+        comments = post.comments.paginate(page=page, per_page=per_page)
+        if not comments.items:
+            abort(404)
+
+        return {
+            'comments': [comment.to_json()for comment in comments.items],
+            'page': comments.page,
+            'page_size': comments.per_page,
+            'total_page_items': len(comments.items),
+            'prev_page': comments.prev_num,
+            'next_page': comments.next_num,
+            'total': comments.total,
+            'total_pages': comments.pages
+        }, 200
